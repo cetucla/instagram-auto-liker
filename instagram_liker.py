3c4f6e7b2111a17b83d4193144f6080f15456ed2
Anthony Humay <ahumay@Anthonys-MacBook-Air.local>
2019-03-24 00:58:16 -0700

    smd

--- instagram_liker.py
+++ instagram_liker.py
@@ -1,44 +1,62 @@
-import pyautogui as pag
+import pyautogui
+import cv2
+import numpy as np
 import time
-from imagesearch import *
 
-screenWidth, screenHeight = pag.size()
+screenWidth, screenHeight = pyautogui.size()
 
 print("Screen Width: ", screenWidth)
 print("Screen Height: ", screenHeight)
 
 def open_chrome():
-	pag.hotkey('command', 'space')
-	time.sleep(.1)
-	pag.typewrite(['c', 'h', 'r', 'o', 'm', 'e', 'enter'], interval=.01)
+	time.sleep(.5)
+	pyautogui.hotkey('command', 'space')
+	time.sleep(.5)
+	pyautogui.typewrite(['s', 'a', 'f', 'a', 'r', 'i', 'enter'], interval=.1)
 	time.sleep(1)
-	open_instagram()
+
+def full_screen():
+	pyautogui.hotkey('ctrl', 'command', 'f')
+	time.sleep(1)
 
 def open_instagram():
-	pag.moveTo(310, 78)
-	pag.click()
+	pyautogui.moveTo(700, 20)
+	pyautogui.click()
 	time.sleep(.1)
-	pag.hotkey('ctrl', 'a')
+	pyautogui.hotkey('ctrl', 'a')
 	time.sleep(1)
-	pag.typewrite(['i', 'n', 's', 't', 'a', 'g', 'r', 'a', 'm', 'enter'], interval=.01)
-	time.sleep(.1)
+	pyautogui.typewrite(['backspace', 'i', 'n', 's', 't', 'a', 'g', 'r', 'a', 'm', '.', 'c', 'o', 'm', 'enter'], interval=.1)
+	time.sleep(3)
 
-	pos = imagesearcharea("likebutton.png", 0, 210, 1680, 1050)
-	if pos[0] != -1:
-		print("position : ", pos[0], pos[1])
-		pyautogui.moveTo(pos[0], pos[1])
-	else:
-		print("image not found")
+def findLikes():
+	imag = pyautogui.screenshot(region=(0, 150, 1430, 898))
+	imag.save("suckmyballs.png")
+	img_rgb = cv2.imread('suckmyballs.png')
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+	template = cv2.imread('icon.png', 0)
+	w, h = template.shape[::-1]
+
+	res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+	threshold = 0.7
+	loc = np.where( res >= threshold)
+	for pt in zip(*loc[::-1]):
+		print("heart found at x : " + pt[0] + w + " and y : " + pt[1] + h)
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+	cv2.imwrite('Detected.png',img_rgb)
 
 def like_photos():
-	pag.moveTo(400, 1025)
+	pyautogui.moveTo(400, 1025)
 	for i in range(5):
 		time.sleep(1)
-		pag.scroll(-100)
-		#pag.click()
-
+		pyautogui.scroll(-100)
+		#pyautogui.click()
 
 
 if __name__ == "__main__":
 	open_chrome()
+	full_screen()
+	open_instagram()
+	findLikes()
 	# like_photos()
