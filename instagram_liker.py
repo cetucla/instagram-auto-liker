ccf24ee472dbd5621fc111aec162efb24ba1a6fc
Cody Swain <swain.cody@gmail.com>
2019-03-24 00:36:36 -0700

    Removed async stuff

--- instagram_liker.py
+++ instagram_liker.py
@@ -1,18 +1,16 @@
-import pyautogui as pag
 import time
-# from imagesearch import *
 import asyncio
-
 import cv2
 import numpy as np
+import pyautogui as pag
 
 screenWidth, screenHeight = pag.size()
-
 print("Screen Width: ", screenWidth)
 print("Screen Height: ", screenHeight)
 
 def capture_region(x1, y1, x2, y2):
     img = pag.screenshot(region=(x1,y1,x2,y2))
+    # For debugging, uncomment the following
     # img.save("captured_region.png")
     return img
 
@@ -20,31 +18,9 @@ def get_image_location(template_img_name, search_img, method):
     search_img_rgb = np.array(search_img)
     search_img_gray = cv2.cvtColor(search_img_rgb, cv2.COLOR_BGR2GRAY)
     template_img = cv2.imread(template_img_name, 0)
-
     res = cv2.matchTemplate(search_img_gray, template_img, method)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     return max_loc
-
-    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    # if max_val < .8:
-    #     return [-1, -1]
-    # return max_loc
-
-
-async def open_chrome():
-	pag.hotkey('command', 'space')
-	await asyncio.sleep(.4)
-	pag.typewrite(['c', 'h', 'r', 'o', 'm', 'e', 'enter'], interval=.01)
-	await asyncio.sleep(1)
-
-async def open_instagram():
-	pag.moveTo(310, 78)
-	pag.click()
-	await asyncio.sleep(.1)
-	pag.hotkey('ctrl', 'a')
-	await asyncio.sleep(1)
-	pag.typewrite(['i', 'n', 's', 't', 'a', 'g', 'r', 'a', 'm', 'enter'], interval=.01)
-	await asyncio.sleep(3)
 
 def like_photo():
 	# Weird behavior for pixel width of capturing region vs moving mouse w/ pyautogui
@@ -61,23 +37,35 @@ def like_photo():
 		pag.moveTo(location[0]/2, location[1]/2)
 	'''
 
-	# cv2.TM_CCOEFF method works well 
-	location = get_image_location("likebutton.png", im, cv2.TM_CCOEFF)
+	location = get_image_location("likebutton.png", im, cv2.TM_CCOEFF) # cv2.TM_CCOEFF method works well 
 	if (location[0] != 0):
 		print(location[0], location[1])
 		pag.moveTo(location[0]/2+20, location[1]/2+10+(screenHeight)/6) #divide by 2 bc pyautogui has weird res issues
 
-async def like_all(num):
-	for i in range(num):
-		pag.scroll(-30)
-		await asyncio.sleep(1)
-		like_photo()
-		await asyncio.sleep(2)
+def open_chrome():
+	pag.hotkey('command', 'space')
+	time.sleep(.4)
+	pag.typewrite(['c', 'h', 'r', 'o', 'm', 'e', 'enter'], interval=.01)
+	time.sleep(1)
 
+def open_instagram():
+	pag.moveTo(310, 78)
+	pag.click()
+	time.sleep(.1)
+	pag.hotkey('ctrl', 'a')
+	time.sleep(1)
+	pag.typewrite(['i', 'n', 's', 't', 'a', 'g', 'r', 'a', 'm', 'enter'], interval=.01)
+	time.sleep(3)
+
+def like_n_photos(n):
+	for i in range(n):
+		pag.scroll(-25)
+		time.sleep(1)
+		like_photo()
+		time.sleep(1)
 
 
 if __name__ == "__main__":
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(open_chrome())
-	loop.run_until_complete(open_instagram())
-	loop.run_until_complete(like_all(10))
+	open_chrome()
+	open_instagram()
+	like_n_photos(10)
